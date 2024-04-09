ff66591357dcc183e83466cc7f692895b90bbc3e
alvinhunter7450 <hunteral21@students.ecu.edu>
2024-04-09 12:09:42 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    log = 'index'
+    return render_template('index.html', log_index=log)
+
+@app.route('/GasCheck')
+def gasCheck():
+    log = 'gasCheck'
+    return render_template('GasCheck.html', log_index=log)
+
+@app.route('/login')
+def login():
+    log = 'login'
+    return render_template('login.html', log_index=log)
+
+@app.route('/signup')
+def signup():
+    log = 'login'
+    return render_template('signup.html', log_index=log)
+
+if __name__ == '__main__':
+    app.debug = True
+    ip = '127.0.0.1'
+    app.run(host=ip)
+    
